0643e61dafb135092018b819ce4b029083b455e8
github username <github email address>
2017-04-29 12:54:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much."
+
+var dodger = $('#dodger')
+
+function moveDodgerRight(){
+  var rightNumber = dodger.style.left.replace('px', '')
+  var right = parseInt(rightNumber)
+
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e){
+  if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
